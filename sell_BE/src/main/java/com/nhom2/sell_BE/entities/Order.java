31ce57cf9c6d89e25875d86339fd721d726e9e1a
tvtoi <tvtoiit@gmail.com>
2023-09-25 07:31:12 +0700

    edit entity order

--- sell_BE/src/main/java/com/nhom2/sell_BE/entities/Order.java
+++ sell_BE/src/main/java/com/nhom2/sell_BE/entities/Order.java
@@ -22,10 +22,21 @@ import java.util.Set;
 public class Order implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
     @Id
     @Column(name = "order_id", length = 15, nullable = false)
     private String orderId;
+
+    @Column(name = "user_name", length = 50, nullable = false)
+    private String userName;
+
+    @Column(name = "phone_number", length = 10, nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "gender", length = 1, nullable = false)
+    private boolean gender;
+
+    @Column(name = "address", columnDefinition = "TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci", nullable = false)
+    private String address;
 
     @Column(name = "created_at", nullable = false)
     @CreationTimestamp
