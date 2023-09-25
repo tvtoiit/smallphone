e3169f8994da01f0d8730c70016e1e59894bd538
tvtoi <tvtoiit@gmail.com>
2023-09-25 21:34:04 +0700

    page success order

---
+++ sell_FE/src/pages/SuccessOrder/index.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import classNames from 'classnames/bind';
+import ressut from '../../assets/successorder.png';
+import styles from './SuccessOrder.module.scss';
+import { NavLink } from 'react-router-dom';
+
+
+const cx = classNames.bind(styles);
+function SuccessOrder() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('l-12 m-12 c-12')}>
+                <div className={cx('container-order')}>
+                    <img className={cx('container-img')} src={ressut}/>
+                    <div className={cx('success-text')}>Đặt hàng thành công</div>
+                </div>
+                <div className={cx('button')}>
+                    <NavLink className={cx('nav-button')} to={'/'}>Quay về trang chủ</NavLink>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default SuccessOrder;
